ed4ec9b6475dd98772eaac281f0a654a79aedeb5
Andres Salinas <HeavyUberCharger@gmail.com>
2015-10-05 05:48:07 -0500

    Andres Salinas 10/2/15

---
+++ src/salinas_3_datatypes/Salinas_3_datatypes.java
@@ -0,0 +1,69 @@
+
+package salinas_3_datatypes;
+
+
+public class Salinas_3_datatypes {
+
+    
+    public static void main(String[] args) {
+        //This is a comment
+        //we will use many comments to explain our code
+        //this program explores some of the printive data types 
+        int i = 1;
+        float a = 2.5f;
+        double b = 3.87;
+        short c = 16;
+        char d1 = ';';
+        char d2 = 'a';
+        char d3 = 42;
+        char d4 = 145;
+        String e = d4 +"";
+        String el = "IM THE GREATEST";
+        boolean h = true;
+        boolean g = i % 2 == 0;
+        
+        
+        // here we willl look at some mathematical operators
+        // + this adds 2 numbers and more
+        // - this subtracts 2 numbers and more
+        // * this multiplies 2 numbers and more
+        // / this divides two numbers and more
+        // % this is modulo, it shows the remainder
+        // 
+        
+        System.out.println(i + " this is a integer");
+        System.out.println(a + " this is a a float");
+        System.out.println(b + " this is a double");
+        System.out.println(c + " this is a short");
+        System.out.println(d1 + " this is a char");
+        System.out.println(d2 + " this is a char");
+        System.out.println(d3 + " this is a char");
+        System.out.println(d4 + " this is a char");
+        System.out.println(e + " this is a String");
+        System.out.println(el + " this is a String");
+        System.out.println(h + " this is a boolean");
+        System.out.println(g + " this is a boolean");
+            
+//        for(i = -123; i < 123; i++){
+//           // System.out.println((char) i );
+//            System.out.println(i % 3 == 0);
+//        }
+        i = 7;
+        a = 0;
+        e = "ninja";
+         System.out.println(i+a + " an interger / " + "plus a float");
+        System.out.println(a+ b + " a float/" + "plus a double");
+        System.out.println(b + c + " a double/" + "plus a short");
+        System.out.println(c + " a short");
+        System.out.println(e + i + "a string plus a interger");
+        
+        for(int w = 1; w < 101; w++){
+             
+             } 
+        
+        }
+        
+    }
+    
+
+
